2df4d915e3126cfaf8798e8564de15bac7c73473
nitesh <sabbavarapunitesh636@gmail.com>
2024-10-01 15:28:29 +0530

    created_files

--- niat_project_backend/settings.py
+++ niat_project_backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helpdesk'
+    'niat_auth',
+    'attendance_tracker'
 ]
 
 MIDDLEWARE = [
